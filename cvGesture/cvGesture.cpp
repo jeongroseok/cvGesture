4e89883a96e54edb4252b74bdc74b46434d7e7fc
석정로 <jeongroseok@gmail.com>
2019-05-26 15:33:31 +0900

    Init env with some example

---
+++ cvGesture/cvGesture.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include <iostream>
+
+#define CVUI_IMPLEMENTATION
+#define WINDOW_NAME "CVUI Hello World!"
+#include "cvui.h"
+
+int main()
+{
+	cv::Mat frame = cv::Mat(200, 500, CV_8UC3);
+
+	cvui::init(WINDOW_NAME);
+	uint8_t count = 0;
+
+	while (true) {
+		frame = cv::Scalar(49, 52, 49);
+
+		cvui::text(frame, 110, 40, "Hello, world!");
+		if (cvui::button(frame, 110, 70, "Button")) {
+			count++;
+		}
+		cvui::printf(frame, 110, 100, "%d", count);
+
+		cvui::imshow(WINDOW_NAME, frame);
+
+		if (cv::waitKey(20) == 27) {
+			break;
+		}
+	}
+
+	return 0;
+}
